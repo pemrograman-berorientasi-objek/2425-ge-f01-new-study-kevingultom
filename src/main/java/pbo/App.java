a82a09d612589ac9d5d87d6993ccf08ef672389f
Kevin Gultom <143678985+kevingultom@users.noreply.github.com>
2025-05-20 20:11:00 +0700

    test

--- src/main/java/pbo/App.java
+++ src/main/java/pbo/App.java
@@ -1,66 +1,44 @@
 package pbo;
 
-/**
- *
+/*
  * 12S23001 Kevin Gultom
  * 12S23010 Tiffani Butar-butar
- * 
  */
 
-import java.util.*;
 import javax.persistence.*;
-import pbo.model.*;
+import java.util.Scanner;
+import pbo.model.Executor;
 
 public class App {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("f01");
+    private static final EntityManager em = emf.createEntityManager();
+    private static final Executor executor = new Executor(em);
 
-  private static EntityManagerFactory factory;
-  private static EntityManager entityManager;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equals("---")) break;
 
-  public static void main(String[] args) {
-    factory = Persistence.createEntityManagerFactory("study_plan_pu");
-    entityManager = factory.createEntityManager();
-    drivApp.initializeEntityManager();
-    drivApp.cleanTableStudent();
-    drivApp.cleanTableCourse();
-    drivApp.cleanTableEnrollment();
-
-    String str;
-
-    Scanner scanner = new Scanner(System.in);
-
-    while (true) {
-      str = scanner.nextLine();
-
-      if (str.equals("---")) {
-        break;
-      } else {
-        String split[] = str.split("#");
-
-        switch (split[0]) {
-          case "student-add":
-            drivApp.CreateStudent(split[1], split[2], split[3]);
-            break;
-          case "student-show-all":
-            drivApp.ShowStudent();
-            break;
-          case "course-add":
-            drivApp.createCourse(split[1], split[2], Integer.parseInt(split[3]), Integer.parseInt(split[4]));
-            break;
-          case "course-show-all":   
-            drivApp.ShowCourse();
-            break;
-          case "enroll":
-            drivApp.createEnroll(split[1], split[2]);
-            break;
-          case "student-show":
-            drivApp.ShowStudentDetail(split[1]);
-            break;
-          default:
-            break;
+            if (input.startsWith("student-add#")) {
+                String[] tokens = input.split("#");
+                executor.addStudent(tokens[1], tokens[2], tokens[3]);
+            } else if (input.equals("student-show-all")) {
+                executor.showAllStudents();
+            } else if (input.startsWith("course-add#")) {
+                String[] tokens = input.split("#");
+                executor.addCourse(tokens[1], tokens[2], Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
+            } else if (input.equals("course-show-all")) {
+                executor.showAllCourses();
+            } else if (input.startsWith("enroll#")) {
+                String[] tokens = input.split("#");
+                executor.enrollStudent(tokens[1], tokens[2]);
+            } else if (input.startsWith("student-show#")) {
+                String nim = input.split("#")[1];
+                executor.showStudentDetail(nim);
+            }
         }
-      }
-
+        em.close();
+        emf.close();
     }
-
-  }
 }
